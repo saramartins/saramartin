d5e01c3253e27d2bc7810ffd753dc938e47c1382
Sara Martin <sara.elisabet.martin@gmail.com>
2021-06-29 23:09:40 +0200

    Adds basic react app with name and title

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+      <header>
+        <h1>
+          Sara Martin
+        </h1>
+        <p>Frontend Developer</p>
+      </header>
+    </div>
+    </div>
+  );
+}
+
+export default App;
